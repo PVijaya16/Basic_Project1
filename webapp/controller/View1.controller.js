6a6ca1bef4a8e4d47fe5dda0f375afdcc2359ff5
Vijaya Porakapalli <vijayareddy036@gmail.com>
2026-04-01 07:26:58 +0000

    new changes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -20,66 +20,68 @@ sap.ui.define([
 
             var oImage = this.byId("myImage");
             if (oImage) {
-                var sPath = sap.ui.require.toUrl("project9/images/OIP.webp");
+                var sPath = sap.ui.require.toUrl("project9/images/OIP.webp"); //setting image path
                 oImage.setSrc(sPath);
             }
 
-            var oInvoiceModel = new JSONModel();
-            oInvoiceModel.loadData(sap.ui.require.toUrl("project9/model/invoices.json"));
-            this.getView().setModel(oInvoiceModel, "invoiceModel");
+            var oInvoiceModel = new JSONModel(); //creates json model
+            oInvoiceModel.loadData(sap.ui.require.toUrl("project9/model/invoices.json")); //load data from json file
+            this.getView().setModel(oInvoiceModel, "invoiceModel");//attaches model to view with name invoiceModel
         },
 
-        onHomePress: function () {},
+        onHomePress: function () {
+             MessageToast.show("Welcome to Application");
+        },//empty function
 
         onBackPress: function () {
+ 
+            this._viewState = 0;  //reset state this is for success box
 
-            this._viewState = 0;
-
-            this.byId("formContainer").setVisible(true);
+            this.byId("formContainer").setVisible(true); // show form again when we click navigation icon in invoices data
             this.byId("footerBar").setVisible(true);
 
-            this.byId("successContainer").setVisible(false);
+            this.byId("successContainer").setVisible(false);//used to hide other sections
             this.byId("imageContainer").setVisible(false);
             this.byId("invoiceContainer").setVisible(false);
-            this.byId("successBox").setVisible(true);
+            this.byId("successBox").setVisible(true);//shows success box only
         },
 
-        onSubmit: function () {
+        onSubmit: function () { //submit button
 
-            MessageToast.show("Thank You!");
+            MessageToast.show("Thank You!"); //pops up message
 
-            this.byId("formContainer").setVisible(false);
+            this.byId("formContainer").setVisible(false); //hides form and footer section
             this.byId("footerBar").setVisible(false);
-            this.byId("successContainer").setVisible(true);
+            this.byId("successContainer").setVisible(true); //only showa success
 
             this.byId("successBox").setVisible(true);
             this.byId("imageContainer").setVisible(false);
             this.byId("invoiceContainer").setVisible(false);
 
-            this._viewState = 0;
+            this._viewState = 0; //reset state
         },
 
-        onShowImage: function () {
-
+        onShowImage: function () {  //image show
+ 
             var oSuccessBox = this.byId("successBox");
             var oImage = this.byId("imageContainer");
             var oInvoice = this.byId("invoiceContainer");
 
-            this._viewState++;
+            this._viewState++; // move to next state
 
-            if (this._viewState > 2) {
+            if (this._viewState > 2) {  //cycle b/w invoice(2) & image(1)
                 this._viewState = 1;
             }
 
-            if (this._viewState === 1) {
+            if (this._viewState === 1) { //show image
                 oSuccessBox.setVisible(false);
-                oImage.setVisible(true);
+                oImage.setVisible(true); //shows image only
                 oInvoice.setVisible(false);
 
             } else if (this._viewState === 2) {
                 oSuccessBox.setVisible(false);
                 oImage.setVisible(false);
-                oInvoice.setVisible(true);
+                oInvoice.setVisible(true); //shows invoice only
             }
         },
 
@@ -87,7 +89,7 @@ sap.ui.define([
 
             this._viewState = 0;
 
-            this.byId("successBox").setVisible(true);
+            this.byId("successBox").setVisible(true); //aftr clicking previous button show only successbox
             this.byId("imageContainer").setVisible(false);
             this.byId("invoiceContainer").setVisible(false);
         },
@@ -97,7 +99,7 @@ sap.ui.define([
             this.byId("formContainer").setVisible(true);
             this.byId("headerBar").setVisible(true);
             this.byId("footerBar").setVisible(true);
-            this.byId("successContainer").setVisible(false);
+            this.byId("successContainer").setVisible(false); //hides success message
 
             this.byId("imageContainer").setVisible(false);
             this.byId("invoiceContainer").setVisible(false);
